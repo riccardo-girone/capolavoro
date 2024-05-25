d294d3da83116f24232fd1587156431dd837a73a
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-05-25 16:24:14 +0200

    feat: changed to tailwind to scss title section

--- capolavoro/src/app/not-found.js
+++ capolavoro/src/app/not-found.js
@@ -1,25 +1,25 @@
 import Link from 'next/link'
 import styles from './not-found.module.css'
- 
-export default function NotFound() {
-  return (
-    <div className={styles.content}>
-      <div className={styles.mainContent}>
-        <div className={styles.error}>404</div>
-        <div className={styles.title}>
-          <div>PAGE</div>
-          <div>NOT</div>
-          <div>FOUND</div>
-        </div>
-        <div className={styles.subTitle}>
-          <div>Sorry, what are you doing here?!</div>
-          <div>LET US COOK</div>
-        </div>
-      </div>
 
-      <div className={styles.back}>
-        <Link href="/"> ← Back to home</Link>
-      </div>
-    </div>
-  )
+export default function NotFound() {
+    return (
+        <div className={styles.content}>
+            <div className={styles.mainContent}>
+                <div className={styles.error}>404</div>
+                <div className={styles.title}>
+                    <div>PAGE</div>
+                    <div>NOT</div>
+                    <div>FOUND</div>
+                </div>
+                <div className={styles.subTitle}>
+                    <div>Sorry, what are you doing here?!</div>
+                    <div>LET US COOK</div>
+                </div>
+            </div>
+
+            <div className={styles.back}>
+                <Link href="/"> ← Back to home</Link>
+            </div>
+        </div>
+    )
 }
--- capolavoro/src/components/Home/About/About.jsx
+++ capolavoro/src/components/Home/About/About.jsx
@@ -1,25 +1,27 @@
+import styles from './About.module.scss'
+
 const About = () => {
     return(
-        <div className='h-screen w-2/5 bg-color-primary flex items-start justify-center flex-col pl-72 pr-72'>
-            <div className='mb-80'>
-                <div className='text-number-fontsize text-color-white font-primary font-black leading-eleven-height mb-16'>11</div>
-                <div className='flex items-start flex-col font-primary'>
-                    <div className='text-color-white font-extrabold text-sub-titel m-0 leading-10'>PERSONAL BEST</div>
-                    <div className='text-color-white font-extrabold text-sub-titel m-0 leading-10'>MANGA</div>
-                    <div className='text-color-white font-extrabold text-sub-titel m-0 leading-10'>PANEL</div>
+        <div className={styles.contentContainer}>
+            <div className={styles.titleWrapper}>
+                <div className={styles.titleNum}>11</div>
+                <div className={styles.title}>
+                    <div>PERSONAL BEST</div>
+                    <div>MANGA</div>
+                    <div>PANEL</div>
                 </div>
             </div>
 
-            <div className='flex items-center justify-center gap-3 mb-48'>
-                <div className="h-3.5 w-3.5 bg-color-white rounded-full"></div>
-                <div className='text-color-white font-secondary font-normal text-ex-fontsize'>MY PERSONAL EXPERIENCE</div>
+            <div className={styles.myEx}>
+                <div className={styles.circle}></div>
+                <div className={styles.myExTitle}>MY PERSONAL EXPERIENCE</div>
             </div>
 
-            <div className='flex flex-col gap-4 max-w-96'>
-                <div className='text-color-white font-secondary font-light text-normal-fontsize'>
+            <div className={styles.Explenetion}>
+                <div>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et interdum neque. Praesent id 
                 </div>
-                <div className='text-color-white font-secondary font-light text-normal-fontsize'>
+                <div>
                     Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
                 </div>
             </div>
