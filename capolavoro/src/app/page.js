fe2bad677c2f16ed977f7a0cf2df7d630fec8489
riccardo-girone <riccardoo.gironee@gmail.com>
2024-05-30 08:39:08 +0200

    feat: added smoot scroll

--- capolavoro/src/app/page.js
+++ capolavoro/src/app/page.js
@@ -1,6 +1,17 @@
+'use client'
 import HomePage from "../components/Home/Home.jsx";
+import { useEffect } from "react";
 
 const Home = () => {
+    useEffect( () => {
+        (
+          async () => {
+              const LocomotiveScroll = (await import('locomotive-scroll')).default
+              const locomotiveScroll = new LocomotiveScroll();
+          }
+        )()
+      }, [])
+
     return (
         <HomePage/>
     );
